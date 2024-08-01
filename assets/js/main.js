cb98afda949e6c08f34178568a6390ba6c4e121d
Arjun Pankhaniya <arjun.pankhaniya@gmail.com>
2024-08-01 18:30:42 +0530

    Updating Site
    Updating Skills, Education Details, Resume and more.

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,26 +29,26 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 1800,
+    duration: 1700,
     reset: true
 });
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__img',{delay: 380}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__subtitle',{delay: 350}); 
+sr.reveal('.about__text',{delay: 350}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 550});
+sr.reveal('.skills__data',{interval: 180}); 
+sr.reveal('.skills__img',{delay: 400});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 
